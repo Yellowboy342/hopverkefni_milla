a994de537f7def3c79888ae7553253cbb5a57324
snavar98 <sigfus98@gmail.com>
2016-11-29 12:23:47 +0000

    Gerðum byrjun á AI

---
+++ Hopverkefni/Hopverkefni/Ai.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Hopverkefni
+{
+    partial class Ai
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbAi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAi3 = new System.Windows.Forms.Button();
+            this.btnAi2 = new System.Windows.Forms.Button();
+            this.btnAi11 = new System.Windows.Forms.Button();
+            this.btnAi6 = new System.Windows.Forms.Button();
+            this.btnAi5 = new System.Windows.Forms.Button();
+            this.btnAi4 = new System.Windows.Forms.Button();
+            this.btnAi7 = new System.Windows.Forms.Button();
+            this.btnAi8 = new System.Windows.Forms.Button();
+            this.btnAi9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nýrLeikurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbAi
+            // 
+            this.tbAi.BackColor = System.Drawing.Color.White;
+            this.tbAi.Enabled = false;
+            this.tbAi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAi.ForeColor = System.Drawing.Color.Black;
+            this.tbAi.Location = new System.Drawing.Point(523, 321);
+            this.tbAi.Multiline = true;
+            this.tbAi.Name = "tbAi";
+            this.tbAi.Size = new System.Drawing.Size(255, 56);
+            this.tbAi.TabIndex = 41;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(753, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(753, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 25);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(561, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 25);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Gervigreind stig:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(574, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 25);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Leikmaður stig:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 33);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Mylla gervigreind";
+            // 
+            // btnAi3
+            // 
+            this.btnAi3.BackColor = System.Drawing.Color.White;
+            this.btnAi3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi3.Location = new System.Drawing.Point(404, 141);
+            this.btnAi3.Name = "btnAi3";
+            this.btnAi3.Size = new System.Drawing.Size(113, 104);
+            this.btnAi3.TabIndex = 34;
+            this.btnAi3.UseVisualStyleBackColor = false;
+            this.btnAi3.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi2
+            // 
+            this.btnAi2.BackColor = System.Drawing.Color.White;
+            this.btnAi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi2.Location = new System.Drawing.Point(293, 141);
+            this.btnAi2.Name = "btnAi2";
+            this.btnAi2.Size = new System.Drawing.Size(113, 104);
+            this.btnAi2.TabIndex = 33;
+            this.btnAi2.UseVisualStyleBackColor = false;
+            this.btnAi2.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi11
+            // 
+            this.btnAi11.BackColor = System.Drawing.Color.White;
+            this.btnAi11.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi11.Location = new System.Drawing.Point(182, 141);
+            this.btnAi11.Name = "btnAi11";
+            this.btnAi11.Size = new System.Drawing.Size(113, 104);
+            this.btnAi11.TabIndex = 32;
+            this.btnAi11.UseVisualStyleBackColor = false;
+            this.btnAi11.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi6
+            // 
+            this.btnAi6.BackColor = System.Drawing.Color.White;
+            this.btnAi6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi6.Location = new System.Drawing.Point(404, 243);
+            this.btnAi6.Name = "btnAi6";
+            this.btnAi6.Size = new System.Drawing.Size(113, 104);
+            this.btnAi6.TabIndex = 31;
+            this.btnAi6.UseVisualStyleBackColor = false;
+            this.btnAi6.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi5
+            // 
+            this.btnAi5.BackColor = System.Drawing.Color.White;
+            this.btnAi5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi5.Location = new System.Drawing.Point(293, 243);
+            this.btnAi5.Name = "btnAi5";
+            this.btnAi5.Size = new System.Drawing.Size(113, 104);
+            this.btnAi5.TabIndex = 30;
+            this.btnAi5.UseVisualStyleBackColor = false;
+            this.btnAi5.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi4
+            // 
+            this.btnAi4.BackColor = System.Drawing.Color.White;
+            this.btnAi4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi4.Location = new System.Drawing.Point(182, 243);
+            this.btnAi4.Name = "btnAi4";
+            this.btnAi4.Size = new System.Drawing.Size(113, 104);
+            this.btnAi4.TabIndex = 29;
+            this.btnAi4.UseVisualStyleBackColor = false;
+            this.btnAi4.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi7
+            // 
+            this.btnAi7.BackColor = System.Drawing.Color.White;
+            this.btnAi7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi7.Location = new System.Drawing.Point(182, 345);
+            this.btnAi7.Name = "btnAi7";
+            this.btnAi7.Size = new System.Drawing.Size(113, 104);
+            this.btnAi7.TabIndex = 28;
+            this.btnAi7.UseVisualStyleBackColor = false;
+            this.btnAi7.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi8
+            // 
+            this.btnAi8.BackColor = System.Drawing.Color.White;
+            this.btnAi8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi8.Location = new System.Drawing.Point(293, 345);
+            this.btnAi8.Name = "btnAi8";
+            this.btnAi8.Size = new System.Drawing.Size(113, 104);
+            this.btnAi8.TabIndex = 27;
+            this.btnAi8.UseVisualStyleBackColor = false;
+            this.btnAi8.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // btnAi9
+            // 
+            this.btnAi9.BackColor = System.Drawing.Color.White;
+            this.btnAi9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAi9.Location = new System.Drawing.Point(404, 345);
+            this.btnAi9.Name = "btnAi9";
+            this.btnAi9.Size = new System.Drawing.Size(113, 104);
+            this.btnAi9.TabIndex = 26;
+            this.btnAi9.UseVisualStyleBackColor = false;
+            this.btnAi9.Click += new System.EventHandler(this.btnAiClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.IndianRed;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(829, 24);
+            this.menuStrip1.TabIndex = 36;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nýrLeikurToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.menuToolStripMenuItem.Text = "Valmynd";
+            // 
+            // nýrLeikurToolStripMenuItem
+            // 
+            this.nýrLeikurToolStripMenuItem.Name = "nýrLeikurToolStripMenuItem";
+            this.nýrLeikurToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.nýrLeikurToolStripMenuItem.Text = "Nýr leikur";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.exitToolStripMenuItem.Text = "Hætta";
+            // 
+            // Ai
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(829, 499);
+            this.Controls.Add(this.tbAi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAi3);
+            this.Controls.Add(this.btnAi2);
+            this.Controls.Add(this.btnAi11);
+            this.Controls.Add(this.btnAi6);
+            this.Controls.Add(this.btnAi5);
+            this.Controls.Add(this.btnAi4);
+            this.Controls.Add(this.btnAi7);
+            this.Controls.Add(this.btnAi8);
+            this.Controls.Add(this.btnAi9);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Ai";
+            this.Text = "Ai";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAi3;
+        private System.Windows.Forms.Button btnAi2;
+        private System.Windows.Forms.Button btnAi11;
+        private System.Windows.Forms.Button btnAi6;
+        private System.Windows.Forms.Button btnAi5;
+        private System.Windows.Forms.Button btnAi4;
+        private System.Windows.Forms.Button btnAi7;
+        private System.Windows.Forms.Button btnAi8;
+        private System.Windows.Forms.Button btnAi9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nýrLeikurToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ Hopverkefni/Hopverkefni/Ai.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hopverkefni
+{
+    public partial class Ai : Form
+    {
+
+        bool lota = true;
+        int stigx = 0, stigo = 0;
+        int lotuteljari = 0;
+
+        public Ai()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAiClick(object sender, EventArgs e)
+        {
+
+            Button b = (Button)sender;
+            if (lota)
+            {
+                b.Text = "X";
+                b.BackColor = System.Drawing.Color.Red;
+            }
+
+            else
+            {
+                b.Text = "O";
+                b.BackColor = System.Drawing.Color.DarkGreen;
+            }
+
+
+            lota = !lota;
+            b.Enabled = false;
+
+            lotuteljari++;
+
+            atugasigur();
+        }
+        private void atugasigur()
+        {
+            bool sigurvegari = false;
+            //larétt
+            if ((btnAi11.Text == btnAi2.Text) && (btnAi2.Text == btnAi3.Text) && (!btnAi11.Enabled))
+                sigurvegari = true;
+            else if ((btnAi4.Text == btnAi5.Text) && (btnAi5.Text == btnAi6.Text) && (!btnAi4.Enabled))
+                sigurvegari = true;
+            else if ((btnAi7.Text == btnAi8.Text) && (btnAi8.Text == btnAi9.Text) && (!btnAi7.Enabled))
+                sigurvegari = true;
+            //lóðrétt
+            else if ((btnAi11.Text == btnAi4.Text) && (btnAi4.Text == btnAi7.Text) && (!btnAi11.Enabled))
+                sigurvegari = true;
+            else if ((btnAi2.Text == btnAi5.Text) && (btnAi5.Text == btnAi8.Text) && (!btnAi2.Enabled))
+                sigurvegari = true;
+            else if ((btnAi3.Text == btnAi6.Text) && (btnAi6.Text == btnAi9.Text) && (!btnAi3.Enabled))
+                sigurvegari = true;
+            //kross
+            else if ((btnAi11.Text == btnAi5.Text) && (btnAi5.Text == btnAi9.Text) && (!btnAi11.Enabled))
+                sigurvegari = true;
+            else if ((btnAi3.Text == btnAi5.Text) && (btnAi5.Text == btnAi7.Text) && (!btnAi7.Enabled))
+                sigurvegari = true;
+
+
+            if (sigurvegari)
+            {
+                foreach (Control c in Controls)
+                {
+
+                }
+
+                string vann = "";
+                if (lota)
+                {
+                    vann = "O";
+                    for (int i = 0; i < 1; i++)
+                    {
+                        stigo++;
+                        label5.Text = stigo.ToString();
+                        if (stigx == 10)
+                        {
+                            MessageBox.Show("O er sigurvergari leiksins", "O!!!!!!");
+                            stigo = 0;
+                            stigx = 0;
+                        }
+                    }
+                    foreach (Control c in Controls)
+                    {
+                        try
+                        {
+                            lotuteljari = 0;
+                            btnAi11.Text = "";
+                            btnAi2.Text = "";
+                            btnAi3.Text = "";
+                            btnAi4.Text = "";
+                            btnAi5.Text = "";
+                            btnAi6.Text = "";
+                            btnAi7.Text = "";
+                            btnAi8.Text = "";
+                            btnAi9.Text = "";
+                            Button b = (Button)c;
+                            b.Enabled = true;
+                            b.BackColor = System.Drawing.Color.White;
+                        }
+                        catch { }
+                    }
+                }
+                else
+                {
+                    vann = "X";
+                    for (int i = 0; i < 1; i++)
+                    {
+                        stigx++;
+                        label2.Text = stigx.ToString();
+                        if (stigx == 10)
+                        {
+                            MessageBox.Show("X er sigurvergari leiksins", "X!!!!!!");
+                            stigo = 0;
+                            stigx = 0;
+                        }
+                    }
+
+                    foreach (Control c in Controls)
+                    {
+
+                        try
+                        {
+
+                            lotuteljari = 0;
+                            btnAi11.Text = "";
+                            btnAi2.Text = "";
+                            btnAi3.Text = "";
+                            btnAi4.Text = "";
+                            btnAi5.Text = "";
+                            btnAi6.Text = "";
+                            btnAi7.Text = "";
+                            btnAi8.Text = "";
+                            btnAi9.Text = "";
+                            Button b = (Button)c;
+                            b.Enabled = true;
+                            b.BackColor = System.Drawing.Color.White;
+                        }
+                        catch { }
+                    }
+                }
+
+                MessageBox.Show(vann + " vann þessa lotu");
+                tbAi.Text = vann + " vann síðustu lotu";
+            }
+            else
+            {
+                if (lotuteljari == 9)
+                {
+
+                    tbAi.Text = ("Jafntefli");
+                    foreach (Control c in Controls)
+                    {
+                        try
+                        {
+                            lotuteljari = 0;
+                            btnAi11.Text = "";
+                            btnAi2.Text = "";
+                            btnAi3.Text = "";
+                            btnAi4.Text = "";
+                            btnAi5.Text = "";
+                            btnAi6.Text = "";
+                            btnAi7.Text = "";
+                            btnAi8.Text = "";
+                            btnAi9.Text = "";
+                            Button b = (Button)c;
+                            b.Enabled = true;
+                            b.BackColor = System.Drawing.Color.White;
+                        }
+                        catch { }
+                    }
+
+                }
+            }
+        }
+    }
+}
